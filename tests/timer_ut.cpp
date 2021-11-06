0370521b2ab716f1bf3f38376e56cdca16bf2546
Kuba-Wi <winczukjakub@gmail.com>
2021-11-06 22:07:06 +0100

    timer test improved

--- tests/timer_ut.cpp
+++ tests/timer_ut.cpp
@@ -7,18 +7,42 @@ struct Fake {};
 template <>
 void timer_function<Fake>::operator()() {}
 
-TEST(timerTest, timerShouldStartAndStopWithoutCrash) {
-    constexpr size_t execution_count = 10;
-    Fake fake;
-    timer<Fake> time;
+TEST(timerTest, timerShouldBeStartedAfterStart) {
+    constexpr size_t interval_ms = 1;
+    Fake fake_object;
+    timer<Fake> timer;
 
-    time.stop_timer();
-    for (size_t i = 1; i < execution_count; ++i) {
-        time.start_timer(timer_function{fake}, i);
-    }
+    timer.start_timer(timer_function{fake_object}, interval_ms);
+    ASSERT_TRUE(timer.is_started());
+}
 
-    for (size_t i = 1; i < execution_count; ++i) {
-        time.start_timer(timer_function{fake}, i);
-        time.stop_timer();
-    }
+TEST(timerTest, timerShouldBeStoppedAfterStop) {
+    constexpr size_t interval_ms = 1;
+    Fake fake_object;
+    timer<Fake> timer;
+
+    ASSERT_FALSE(timer.is_started());
+
+    timer.start_timer(timer_function{fake_object}, interval_ms);
+    timer.stop_timer();
+    ASSERT_FALSE(timer.is_started());
+}
+
+TEST(timerTest, getCurrentIntervalShouldReturnTimeIntervalWhenTimerIsStarted) {
+    constexpr size_t interval_ms = 1;
+    Fake fake_object;
+    timer<Fake> timer;
+
+    timer.start_timer(timer_function{fake_object}, interval_ms);
+    auto current_interval_opt = timer.get_current_interval_ms();
+
+    ASSERT_TRUE(current_interval_opt.has_value());
+    ASSERT_EQ(current_interval_opt.value(), interval_ms);
+}
+
+TEST(timerTest, getCurrentIntervalShouldNotReturnTimeIntervalWhenTimerIsStopped) {
+    timer<Fake> timer;
+
+    auto current_interval_opt = timer.get_current_interval_ms();
+    ASSERT_FALSE(current_interval_opt.has_value());
 }
