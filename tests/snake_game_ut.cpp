96202892532d44d7f0289edb950af6b6aedf50b4
Kuba-Wi <winczukjakub@gmail.com>
2021-11-06 23:30:36 +0100

    fixed snake_game tests

--- tests/snake_game_ut.cpp
+++ tests/snake_game_ut.cpp
@@ -15,13 +15,10 @@ struct snakeGameTest : public Test {
     std::unique_ptr<TimerMock> mock = std::make_unique<TimerMock>();
 };
 
-TEST_F(snakeGameTest, constructorShouldSetBoardSizesTimeIntervalAndSnake) {
-    EXPECT_CALL(*mock, stop_timer()).Times(1);
-    snake_game s_game{std::move(mock), interval_ms, height, width};
-
-    ASSERT_EQ(s_game.get_board_height(), height);
-    ASSERT_EQ(s_game.get_board_width(), width);
-    ASSERT_EQ(s_game.get_snake_move_time_ms(), interval_ms);
+TEST_F(snakeGameTest, setBoardSizeShouldSetBoardSizesAndResetSnake) {
+    EXPECT_CALL(*mock, stop_timer()).Times(2);
+    snake_game s_game{std::move(mock)};
+    s_game.set_board_size(height, width);
 
     constexpr size_t data_size = 6;
     auto data = s_game.get_snake_data();
@@ -36,22 +33,31 @@ TEST_F(snakeGameTest, constructorShouldSetBoardSizesTimeIntervalAndSnake) {
     ASSERT_NE(food, tail);
 }
 
-TEST_F(snakeGameTest, startNewGameShouldStopAndStartTimer) {
-    EXPECT_CALL(*mock, start_timer(_, interval_ms)).Times(1);
-    EXPECT_CALL(*mock, stop_timer()).Times(2);
-    snake_game s_game{std::move(mock), interval_ms, height, width};
-    s_game.restart_game();
+TEST_F(snakeGameTest, restartGameShouldStopAndStartTimer) {
+    EXPECT_CALL(*mock, start_timer(_, interval_ms));
+    EXPECT_CALL(*mock, stop_timer()).Times(3);
+    snake_game s_game{std::move(mock)};
+    s_game.set_board_size(height, width);
+    s_game.restart_game(interval_ms);
 }
 
 TEST_F(snakeGameTest, multipleStartNewGameShouldStartTimerMultipleTimes) {
     constexpr size_t start_calls_count = 10;
-    constexpr size_t stop_calls_count = start_calls_count + 1;
+    constexpr size_t stop_calls_count = start_calls_count + 2;
 
     EXPECT_CALL(*mock, start_timer(_, interval_ms)).Times(start_calls_count);
     EXPECT_CALL(*mock, stop_timer()).Times(stop_calls_count);
-    snake_game s_game{std::move(mock), interval_ms, height, width};
+    snake_game s_game{std::move(mock)};
+    s_game.set_board_size(height, width);
 
     for (size_t i = 0; i < start_calls_count; ++i) {
-        s_game.restart_game();
+        s_game.restart_game(interval_ms);
     }
+}
+
+TEST_F(snakeGameTest, snakeShouldNotBeStartedWhenBoardSizesAreNotSet) {
+    EXPECT_CALL(*mock, start_timer(_, _)).Times(0);
+    EXPECT_CALL(*mock, stop_timer()).Times(2);
+    snake_game s_game{std::move(mock)};
+    s_game.restart_game(interval_ms);
 }
